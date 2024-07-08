0d34c5a3bcacfcc31db4a40d4281423e261dcc88
guo <18561933129@163.com>
2024-07-08 14:36:21 +0800

    fix: 修复打包问题

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,2 @@
-// // 类型判断
-// import is from '@/utils/is'
-// // 日期相关
-// import dateTime from "@/utils/date";
-//
-// export default {
-//   dateTime,
-//   is
-// }
-
-
 export { default as is } from '@/utils/is'
 export { default as dateTime} from '@/utils/date'
